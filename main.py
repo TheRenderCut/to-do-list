b2874f46af6b0b7b34513935f19217ad45fc401b
Sripranav Nagabhatla <sripranavnagabhatla@gmail.com>
2026-05-18 19:45:56 +0530

    v1.2: Remove Tasks feature added

--- main.py
+++ main.py
@@ -7,24 +7,47 @@ def add_tasks(tasks):
 def view_tasks(tasks):
     tsks_avl = len(tasks)
     print(f"You have {tsks_avl} tasks: ")
-    for tsk in tasks:
-        print(f"[ ] {tsk}")
-print("-----TO DO LIST-----")
-tasks = []
-user_choice = 0
-while user_choice != 3:
-    print("To-Do List Operations: ")
-    print("1. Add Task")
-    print("2. View Task")
-    print("3. Exit")
-    user_choice = int(input("Select your actions: "))
-    match user_choice:
-        case 1:
-            add_tasks(tasks)
-        case 2:
-            view_tasks(tasks)
-        case 3:
-            print("Exiting....!")
-            break
-        case _:
-            print("Choose only from the given options!")
+    if tsks_avl == 0:
+            print("No available tasks!")
+            return
+    for index, tsk in enumerate(tasks, start = 1):
+        print(f"{index}. [ ] {tsk}")
+def remove_tasks(tasks):
+    tsks_avl = len(tasks)
+    print(f"You have {tsks_avl} tasks: ")
+    if tsks_avl == 0:
+        print("No available tasks!")
+        return
+    for index, tsk in enumerate(tasks, start = 1):
+        print(f"{index}. [ ] {tsk}")
+    task_no = int(input("Enter the task number you want to remove: "))
+    while task_no > tsks_avl or task_no <= 0:
+        print("Enter available task index!")
+        task_no = int(input("Enter the task number you want to remove: "))
+    indx = task_no - 1
+    print(f"Removed task {task_no}: {tasks[indx]}!")
+    tasks.pop(indx)
+def menu():
+    print("-----TO DO LIST-----")
+    tasks = []
+    user_choice = 0
+    while user_choice != 4:
+        print("To-Do List Operations: ")
+        print("1. Add Task")
+        print("2. View Task")
+        print("3. Remove Task")
+        print("4. Exit")
+        user_choice = int(input("Select your actions: "))
+        match user_choice:
+            case 1:
+                add_tasks(tasks)
+            case 2:
+                view_tasks(tasks)
+            case 3:
+                remove_tasks(tasks)
+            case 4:
+                print("Exiting....!")
+                break
+            case _:
+                print("Choose only from the given options!")
+menu()
